b6a6911cc6bfbf6575c28388d6cb82357e7c5181
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-01-30 17:54:00 -0500

    begin azure migration

--- pages/api/server.js
+++ pages/api/server.js
@@ -3,19 +3,38 @@
 
 import { Configuration, OpenAIApi } from "openai";
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+// const configuration = new Configuration({
+//     apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+
+const apiKey = process.env.AZURE_OPENAI_KEY;
+const base_url = process.env.BASE_URL;
+const deploymentName = process.env.DEPLOYMENT_NAME;
+
+let url = `${base_url}/openai/deployments/${deploymentName}/completions?api-version=2022-12-01`
 
 export default async function (req, res) {
-    const completion = await openai.createCompletion({
-        model: "text-davinci-002",
-        prompt: generatePrompt(req.body.prompt),
-        temperature: 0.6,
-        max_tokens: 1000,
+    // const completion = await openai.createCompletion({
+    //     model: "text-davinci-002",
+    //     prompt: generatePrompt(req.body.prompt),
+    //     temperature: 0.6,
+    //     max_tokens: 1000,
+    // });
+    // res.status(200).json({ result: completion.data.choices[0].text });
+
+    const completion = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'api-key': apiKey
+        },
+        body: JSON.stringify(generatePrompt(req.body.prompt))
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
+
+    const response = await completion.json();
+    console.log(response);
+    // res.status(200).json({ result: completion?.choices[0].text });
 }
 
 function generatePrompt(prompt) {
