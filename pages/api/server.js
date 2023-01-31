f3462e174b0dc70a71b3d49e19beecd9dcf807a1
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-01-30 21:08:44 -0500

    server now makes a POST to azure GPT-3 instance

--- pages/api/server.js
+++ pages/api/server.js
@@ -1,13 +1,4 @@
 /* eslint-disable import/no-anonymous-default-export */
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { Configuration, OpenAIApi } from "openai";
-
-// const configuration = new Configuration({
-//     apiKey: process.env.OPENAI_API_KEY,
-// });
-// const openai = new OpenAIApi(configuration);
-
 const apiKey = process.env.AZURE_OPENAI_KEY;
 const base_url = process.env.BASE_URL;
 const deploymentName = process.env.DEPLOYMENT_NAME;
@@ -15,43 +6,31 @@ const deploymentName = process.env.DEPLOYMENT_NAME;
 let url = `${base_url}/openai/deployments/${deploymentName}/completions?api-version=2022-12-01`
 
 export default async function (req, res) {
-    // const completion = await openai.createCompletion({
-    //     model: "text-davinci-002",
-    //     prompt: generatePrompt(req.body.prompt),
-    //     temperature: 0.6,
-    //     max_tokens: 1000,
-    // });
-    // res.status(200).json({ result: completion.data.choices[0].text });
-
-    const completion = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'api-key': apiKey
-        },
-        body: JSON.stringify(generatePrompt(req.body.prompt))
-    });
-
-    const response = await completion.json();
-    console.log(response);
-    res.status(200).json({ result: response.choices[0].text });
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'api-key': apiKey
+            },
+            body: JSON.stringify(generatePrompt(req.body.prompt))
+        });
+    
+        if (!response.ok) {
+            console.log(`HTTP Code: ${response.status} - ${response.statusText}`);
+        } else {
+            const completion = await response.json();
+            res.status(200).json({ result: completion.choices[0].text });
+        }
+    } catch(e) {
+        console.error(e);
+    }
 }
 
 function generatePrompt(prompt) {
-    //   const capitalizedAnimal =
-    //     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-    //   return `Suggest three names for an animal that is a superhero.
-
-    // Animal: Cat
-    // Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-    // Animal: Dog
-    // Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-    // Animal: ${capitalizedAnimal}
-    // Names:`;
-
-    // return `${prompt}`;
     return {
         'prompt': prompt,
         'max_tokens': 1000,
+        // other options here
     };
 }
