de469facd2b986fd5ca17fac86a247624e874571
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-02-09 11:23:29 -0500

    change preload types to font/woff2, and add format to src in globals.css

--- pages/_document.js
+++ pages/_document.js
@@ -7,21 +7,21 @@ export default function Document() {
                 rel="preload"
                 href="/fonts/GT-SD-lt-it.woff2"
                 as="font"
-                type="font/otf"
+                type="font/woff2"
                 crossOrigin=""
             />
             <link
                 rel="preload"
                 href="/fonts/GT-SD-md-it.woff2"
                 as="font"
-                type="font/otf"
+                type="font/woff2"
                 crossOrigin=""
             />
             <link
                 rel="preload"
                 href="/fonts/Monaco.woff2"
                 as="font"
-                type="font/ttf"
+                type="font/woff2"
                 crossOrigin=""
             />
             <Head>
