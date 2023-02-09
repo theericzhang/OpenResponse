52418ebaf663e117608f30c935c18aed175525e6
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-02-08 21:12:00 -0500

    font prefetching properly wired

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,34 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <link
+                rel="preload"
+                href="/fonts/GT-SD-lt-it.otf"
+                as="font"
+                type="font/otf"
+                crossOrigin=""
+            />
+            <link
+                rel="preload"
+                href="/fonts/GT-SD-md-it.otf"
+                as="font"
+                type="font/otf"
+                crossOrigin=""
+            />
+            <link
+                rel="preload"
+                href="/fonts/Monaco.ttf"
+                as="font"
+                type="font/ttf"
+                crossOrigin=""
+            />
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
