dfde1137c6225b04a662e3abfc4d13a6435569c2
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-09-15 16:47:52 -0400

    add div in return statement to stop build fails

--- components/elements/dialoguebox/StatusIndicator.jsx
+++ components/elements/dialoguebox/StatusIndicator.jsx
@@ -1,10 +1,8 @@
 export default function StatusIndicator() {
     let statuses = [
-        'OpenResponse is operational',
-        'OpenResponse is experiencing degraded performance',
-        'Active Incident'
-    ]
-    return (
-        
-    );
+        "OpenResponse is operational",
+        "OpenResponse is experiencing degraded performance",
+        "Active Incident",
+    ];
+    return <div>test</div>;
 }
