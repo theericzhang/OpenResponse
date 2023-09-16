09efbd356e4892b03be468fc4089f0a182aeb844
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-09-16 13:24:41 -0400

    skeletal framework for status indicator dot thingy. dot takes in a status parameter which is filled by fillColors

--- components/elements/dialoguebox/StatusIndicator.jsx
+++ components/elements/dialoguebox/StatusIndicator.jsx
@@ -5,6 +5,25 @@ export default function StatusIndicator() {
         "OpenResponse is experiencing degraded performance",
         "Active Incident",
     ];
-    return <div class={style["status-indicator-wrapper"]}>test</div>;
+    let fillColors = {
+        operational: "#67CE67",
+        degraded: "#CF7914",
+        incedent: "#FF3131",
+    };
+    return (
+        <div class={style["status-indicator-wrapper"]}>
+            <svg
+                width="8"
+                height="8"
+                viewBox="0 0 8 8"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <circle cx="4" cy="4" r="4" fill={fillColors["degraded"]} />
+                {/* TODO: watch fillColors and change colors accordingly to status of API. */}
+            </svg>
+            test
+        </div>
+    );
     // TODO: Create styles for this status indicator - move position to bottom right according to figma doc
 }
