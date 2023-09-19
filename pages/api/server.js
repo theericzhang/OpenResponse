246e258adc37eb9ad6c75eced5bfd5b38662f411
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-09-19 16:53:15 -0400

    change some error message naming

--- pages/api/server.js
+++ pages/api/server.js
@@ -24,9 +24,9 @@ if (isCurrentEnvironmentAzure) {
 }
 
 const httpMessages = {
-    400: "Bad Request - The server did not understand the request, or the request was invalid.",
-    401: "Unauthorized - Authentication is required to access the requested resource. Check to see if your API keys are correct",
-    403: "Forbidden - The server understood the request, but you do not have permission to access the resource.",
+    400: "Your request could not be understood. Try reworking your prompt and resubmitting it",
+    401: "There was a problem authenticating your account. Check to see if your API keys are correct.",
+    403: "Your request was received, but you do not have permission to access the resource.",
     404: "Not Found - The requested resource could not be found on the server.",
     500: "Internal Server Error - An unexpected error occurred on the server.",
 };
